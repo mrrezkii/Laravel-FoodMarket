3b4f40bc0be0ed9331dd5e8db78c855f84a2dc56
nando <muhammad.rezki.ananda@gmail.com>
2021-05-24 00:02:04 +0700

    API Register

--- app/Http/Controllers/APIUserController.php
+++ app/Http/Controllers/APIUserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Actions\Fortify\PasswordValidationRules;
 use Exception;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -11,9 +12,12 @@ use Illuminate\Support\Facades\Hash;
 
 class APIUserController extends Controller
 {
+
+    use PasswordValidationRules;
+
     public function login(Request $request)
     {
-        try{
+        try {
             // Validasi input
             $request->validate([
                 'email' => 'email|required',
@@ -22,7 +26,7 @@ class APIUserController extends Controller
 
             // Mengecek credentials (login)
             $credentials = request(['email', 'password']);
-            if(!Auth::attempt($credentials)) {
+            if (!Auth::attempt($credentials)) {
                 return ResponseFormatter::error([
                     'message' => 'Unauthorized'
                 ], 'Authentication Failed', 500);
@@ -30,7 +34,7 @@ class APIUserController extends Controller
 
             // Jika tidak sesuai maka beri error
             $user = User::where('email', $request->email)->first();
-            if (!Hash::check($request->password, $user->password, [])){
+            if (!Hash::check($request->password, $user->password, [])) {
                 throw new \Exception('Invalid Credentials');
             }
             // Jika berhasil maka loginkan
@@ -43,6 +47,41 @@ class APIUserController extends Controller
         } catch (Exception $error) {
             return ResponseFormatter::error([
                 'message' => 'Something went wrong'
+            ], 'Authentication Falied', 500);
+        }
+    }
+
+    public function register(Request $request)
+    {
+        try {
+            $request->validate([
+                'name' => ['required', 'string', 'max:255'],
+                'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+                'password' => $this->passwordRules()
+            ]);
+
+            User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'address' => $request->address,
+                'houseNumber' => $request->houseNumber,
+                'phoneNumber' => $request->phoneNumber,
+                'city' => $request->city,
+                'password' => Hash::make($request->password),
+            ]);
+
+            $user = User::where('email', $request->email)->first();
+
+            $tokenResult = $user->createToken('authToken')->plainTextToken;
+            return ResponseFormatter::success([
+                'access_token' => $tokenResult,
+                'token_type' => 'Bearer',
+                'user' => $user
+            ]);
+        } catch (Exception $error) {
+            return ResponseFormatter::error([
+                'message' => 'Something went wrong',
+                'error' => $error
             ], 'Authentication Falied', 500);
         }
     }
