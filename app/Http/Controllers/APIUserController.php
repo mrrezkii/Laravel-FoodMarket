4b61e4edab6da1d1fa056fdb212ebc4263805056
nando <muhammad.rezki.ananda@gmail.com>
2021-05-24 07:45:11 +0700

    API Upload Photo

--- app/Http/Controllers/APIUserController.php
+++ app/Http/Controllers/APIUserController.php
@@ -7,8 +7,11 @@ use Exception;
 use App\Models\User;
 use Illuminate\Http\Request;
 use App\Helpers\ResponseFormatter;
+use Illuminate\Auth\Events\Validated;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use League\CommonMark\Extension\SmartPunct\PunctuationParser;
 
 class APIUserController extends Controller
 {
@@ -107,4 +110,33 @@ class APIUserController extends Controller
 
         return ResponseFormatter::success($user, 'Profile Updated');
     }
+
+    public function updatePhoto(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'file' => 'required|image|max:2048'
+        ]);
+
+        if ($validator->fails())
+        {
+            return ResponseFormatter::error(
+                ['error' => $validator->errors()],
+                'Update photo fails',
+                401
+            );
+        }
+
+        if($request->file('file'))
+        {
+            $file = $request->file->store('assets/user', 'public');
+
+            // Simpan foto ke databasenya (urlnya)
+            $user = Auth::user();
+            $user->profile_photo_path = $file;
+            $user->update();
+
+            return ResponseFormatter::success([$file], 'File successfully uploaded');
+        }
+    }
+
 }
