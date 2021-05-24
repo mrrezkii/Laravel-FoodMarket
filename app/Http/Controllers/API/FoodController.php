cf09cc3ddaff8431920652c985f1d726c1f85c8d
nando <muhammad.rezki.ananda@gmail.com>
2021-05-24 22:52:00 +0700

    API Food

--- app/Http/Controllers/API/FoodController.php
+++ app/Http/Controllers/API/FoodController.php
@@ -2,9 +2,73 @@
 
 namespace App\Http\Controllers;
 
+use App\Helpers\ResponseFormatter;
+use App\Models\Food;
 use Illuminate\Http\Request;
 
 class FoodController extends Controller
 {
-    //
+    public function all(Request $request)
+    {
+        $id = $request->input('id');
+        $limit = $request->input('limit', 6);
+        $name = $request->input('name');
+        $type = $request->input('type');
+
+        $price_from = $request->input('price_from');
+        $price_to = $request->input('price_to');
+
+        $rate_from = $request->input('rate_from');
+        $rate_to = $request->input('rate_to');
+
+        if($id)
+        {
+            $food = Food::find($id);
+
+            if($food)
+            {
+                return  ResponseFormatter::success(
+                    $food,
+                    'Data produk berhasil di ambil'
+                );
+            }
+            else
+            {
+                return ResponseFormatter::error(
+                    null,
+                    'Data produk tidak ada',
+                    404
+                );
+            }
+        }
+
+        $food = Food::query();
+
+        if($name)
+        {
+            $food->where('name', 'like', '%' . $name . '%');
+        }
+        if($type)
+        {
+            $food->where('name', 'like', '%' . $type . '%');
+        }
+        if($price_from)
+        {
+            $food->where('price', '>=', $price_from);
+        }
+        if($price_to)
+        {
+            $food->where('price', '<=', $price_to);
+        }
+        if($rate_from)
+        {
+            $food->where('price', '>=', $rate_from);
+        }
+        if($rate_to)
+        {
+            $food->where('price', '<=', $rate_to);
+        }
+        
+        
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FoodController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -24,3 +26,5 @@ Route::middleware('auth:sanctum')->group(function(){
 
 Route::post('login', [UserController::class, 'login']);
 Route::post('register', [UserController::class, 'register']);
+
+Route::get('food', [FoodController::class, 'all']);
